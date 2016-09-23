4fa2f2c0e960a636295989d475d927957d659ffa
Andy  Alameddine <andyalam@hotmail.com>
2016-09-23 11:43:40 -0700

    hooked up file dialog

--- markdown-editor/lib/main.js
+++ markdown-editor/lib/main.js
@@ -1,14 +1,36 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow, dialog } = require('electron');
+const fs = require('fs');
 
 var mainWindow = null;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow();
+  mainWindow = new BrowserWindow({ width: 1000, height: 600 });
   mainWindow.webContents.loadURL(`file://${__dirname}/index.html`);
+
+  mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
 });
+
+
+const showOpenFileDialog = () => {
+  const files = dialog.showOpenDialog(mainWindow, {
+    properties: ['openFile'],
+    filters: [
+      {name: 'Text File', extensions: ['txt'] },
+      {name: 'Markdown', extensions: ['md'] }
+    ]
+  });
+
+  if (!files) { return; }
+
+  const file = files[0];
+  const content = fs.readFileSync(file).toString();
+
+  console.log(content);
+};
+
+
+exports.showOpenFileDialog = showOpenFileDialog;
--- markdown-editor/lib/renderer.js
+++ markdown-editor/lib/renderer.js
@@ -1,14 +1,23 @@
+const { remote } = require('electron');
+const mainProcess = remote.require('./main.js');
+const { showOpenFileDialog } = mainProcess;
+
 const $ = require('jquery');
 const marked = require('marked');
 
-const $markdownView = $('.raw-markdown'); 
+const $markdownView = $('.raw-markdown');
 const $htmlView = $('.rendered-html');
+
 
 
 $markdownView.on('keyup', function() {
   var content = $(this).val();
   renderMarkdownToHtml(content);
 });
+
+$('#open-file').on('click', function() {
+  showOpenFileDialog();
+})
 
 
 function renderMarkdownToHtml(markdown) {
